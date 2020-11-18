5158f0f54db001e6152fa860e401e59c88e63f07
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-11-17 22:54:42 -0800

    rename project to Amicable

--- app/framework.php
+++ app/framework.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Initiator ( framework.php )
+ * Amicable ( framework.php )
  *
  * This file is used to create a new framework instance and adds specific features to the theme.
  *
- * @package     Initiator
- * @copyright   Copyright (C) 2019. Benjamin Lu
+ * @package     Amicable
+ * @copyright   Copyright (C) 2020. Benjamin Lu
  * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
  * @author      Benjamin Lu ( https://benjlu.com )
  */
@@ -15,19 +15,19 @@
  *
  * This will create an instance of the framework allowing you to initialize the theme.
  */
-$initiator = Benlumia007\Backdrop\Framework::get_instance();
+$amicable = Benlumia007\Backdrop\Framework::get_instance();
 
-$initiator->menus = new Benlumia007\Backdrop\Menu\Menu(
+$amicable->menus = new Benlumia007\Backdrop\Menu\Menu(
 	$args = [
-		'primary' => esc_html__( 'Primary Navigation', 'initiator' ),
+		'primary' => esc_html__( 'Primary Navigation', 'amicable' ),
 	]
 );
 
-$initiator->sidebars = new Benlumia007\Backdrop\Sidebar\Sidebar(
+$amicable->sidebars = new Benlumia007\Backdrop\Sidebar\Sidebar(
 	$args = [
 		'primary' => [
-			'name' => esc_html__( 'Primary Sidebar', 'initiator' ),
-			'desc' => esc_html__( 'Test', 'initiator' ),
+			'name' => esc_html__( 'Primary Sidebar', 'amicable' ),
+			'desc' => esc_html__( 'Test', 'amicable' ),
 		],
 	]
 );
--- app/functions-scripts.php
+++ app/functions-scripts.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Initiator ( functions-scripts.php )
+ * Amicable ( functions-scripts.php )
  *
- * @package     Initiator
+ * @package     Amicable
  * @copyright   Copyright (C) 2019. Benjamin Lu
  * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
  * @author      Benjamin Lu ( https://benjlu.com )
@@ -11,7 +11,7 @@
 /**
  * Define namespace
  */
-namespace Initiator;
+namespace Amicable;
 
 /**
  * Enqueue Scripts and Styles
@@ -30,12 +30,12 @@ add_action(
 		 * Rather than enqueue the main stylesheet, we are going to enqueue sceen.css since all of the styles will
 		 * go here. We only need parse the information for the Theme in style.css so that it can be activated.
 		 */
-		wp_enqueue_style( 'initiator-screen', get_theme_file_uri( 'public/css/screen.css' ), array(), '1.0.0' );
+		wp_enqueue_style( 'amicable-screen', get_theme_file_uri( 'public/css/screen.css' ), array(), '1.0.0' );
 
 		/**
 		 * We will be enqueue the app.js file, which mainly be for the navigation only.
 		 */
-		wp_enqueue_script( 'initiator-app', get_theme_file_uri( 'public/js/app.js' ), array('jquery'), '1.0.0', true );
+		wp_enqueue_script( 'amicable-app', get_theme_file_uri( 'public/js/app.js' ), array('jquery'), '1.0.0', true );
 
 		/**
 		 * This allows users to comment by clicking on reply so that it gets nested.
@@ -60,13 +60,13 @@ add_action(
 				{$value}
 			}
 		";
-		wp_add_inline_style( 'initiator-style', $custom_css );
+		wp_add_inline_style( 'amicable-style', $custom_css );
 	}
 );
 
 add_action(
 	'enqueue_block_editor_assets',
 	function() {
-		wp_enqueue_style( 'initiator-custom-fonts', get_theme_file_uri( '/vendor/benlumia007/backdrop-core/assets/fonts/custom-fonts.css' ), array(), '1.0.0' );
+		wp_enqueue_style( 'amicable-custom-fonts', get_theme_file_uri( '/vendor/benlumia007/backdrop-core/assets/fonts/custom-fonts.css' ), array(), '1.0.0' );
 	}
 );

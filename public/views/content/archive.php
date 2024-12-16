fed4da27edf7cf24fe70b437e0bda3e7d6278603
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-12-16 19:48:40 +0000

    make sure the footer credit does not have s gap between the social and credit.

--- public/views/content/archive.php
+++ public/views/content/archive.php
@@ -1,6 +1,16 @@
-<section id="content" class="site-content clear">
+<section id="content" class="site-content">
 	<main id="main" class="content-area">
 		<?php if ( have_posts() ) : ?>
+			<?php if ( ! is_front_page() ) : ?>
+				<div class="archive-header">
+					<h1 class="archive-header__title mb-2"><?php the_archive_title() ?></h1>
+					<?php if ( ! is_paged() && get_the_archive_description() ) : ?>
+						<div class="archive-header__description">
+							<?php the_archive_description() ?>
+						</div>
+					<?php endif ?>
+				</div>
+			<?php endif ?>
 			<div class="loop">
 				<ul class="grid-items grid-col-3">
 					<?php while( have_posts() ) : the_post(); ?>

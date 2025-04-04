059281980d47a526e1c54020f9491559601e2e01
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-04-04 04:45:23 +0000

    make sure it follows the coding standard

--- public/views/entry/archive.php
+++ public/views/entry/archive.php
@@ -2,26 +2,26 @@
 <?php
 	$data = $data->get( 'data' );
 
-	$timestamp = get_the_date( 'U' );
-	$year      = date( 'Y', $timestamp );
-	$month     = date( 'm', $timestamp );
-	$daynum    = date( 'd', $timestamp );
+	$timestamp   = get_the_date( 'U' );
+	$post_year   = gmdate( 'Y', $timestamp );
+	$post_month  = gmdate( 'm', $timestamp );
+	$post_daynum = gmdate( 'd', $timestamp );
 
-	if ( $data->current_year !== $year || $data->current_month !== $month ) {
-		$data->current_year  = $year;
-		$data->current_month = $month;
+	if ( $data->current_year !== $post_year || $data->current_month !== $post_month ) {
+		$data->current_year  = $post_year;
+		$data->current_month = $post_month;
 		$data->current_day   = '';
 
-		echo '<h2 class="archives-site__heading">' . esc_html( date( 'F Y', $timestamp ) ) . '</h2>';
+		echo '<h2 class="archives-site__heading">' . esc_html( gmdate( 'F Y', $timestamp ) ) . '</h2>';
 	}
 
-	$duplicate_day = $data->current_day && $data->current_day === $daynum ? ' day-duplicate' : '';
-	$data->current_day = $daynum;
+	$duplicate_day = $data->current_day && $data->current_day === $post_daynum ? ' day-duplicate' : '';
+	$data->current_day = $post_daynum;
 
 	echo sprintf(
 		'<div class="archives-site__item %s"><span class="archives-site__day">%s:</span> <span class="archives-site__post"><a href="%s">%s</a></span></div>',
 		esc_attr( $duplicate_day ),
-		esc_html( $daynum ),
+		esc_html( $post_daynum ),
 		esc_url( get_permalink() ),
 		esc_html( get_the_title() )
 	);

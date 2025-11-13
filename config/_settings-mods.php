e98376f400858f55511483bd9176ac0922f5b420
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-11-13 16:06:48 +0000

    Rename Backdrop\Theme to Backdrop\ without Theme

--- config/_settings-mods.php
+++ config/_settings-mods.php
@@ -16,7 +16,7 @@
  * @link      https://luthemes.com/portfolio/amicable
  */
 
-use function Backdrop\Theme\is_classicpress;
+use function Backdrop\is_classicpress;
 
 return [
 
@@ -54,12 +54,12 @@ return [
 	// Default footer credit text.
 	'theme_footer_custom_credit' => function() {
 		$year = gmdate( 'Y' ); // Get the current year
-		$copyright = sprintf( __( "&#169; %1\$s. %2\$s.", 'amicable' ), $year, Backdrop\Theme\Site\render_home_link() );
+		$copyright = sprintf( __( "&#169; %1\$s. %2\$s.", 'amicable' ), $year, Backdrop\Site\render_home_link() );
 
 		if ( is_classicpress() ) {
-			$footer_text = sprintf( __( "Powered by %1\$s and %2\$s.", 'amicable' ), Backdrop\Theme\Site\render_cp_link(), Backdrop\Theme\Site\render_theme_link() );
+			$footer_text = sprintf( __( "Powered by %1\$s and %2\$s.", 'amicable' ), Backdrop\Site\render_cp_link(), Backdrop\Site\render_theme_link() );
 		} else {
-			$footer_text = sprintf( __( "Powered by %1\$s and %2\$s.", 'amicable' ), Backdrop\Theme\Site\render_wp_link(), Backdrop\Theme\Site\render_theme_link() );
+			$footer_text = sprintf( __( "Powered by %1\$s and %2\$s.", 'amicable' ), Backdrop\Site\render_wp_link(), Backdrop\Site\render_theme_link() );
 		}
 
 		return $copyright . ' <br /> ' . $footer_text;

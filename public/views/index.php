43dd9db540990821c6858882f8afad189da2a732
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-11-13 16:01:21 +0000

    Update template calls to use Backdrop\Template\hierarchy()

--- public/views/index.php
+++ public/views/index.php
@@ -1,10 +1,10 @@
 <?php
 
 // Loads header/*.php templates
-Backdrop\View\display( 'header', Backdrop\Template\Hierarchy\hierarchy() );
+Backdrop\View\display( 'header', Backdrop\Template\hierarchy() );
 
 // Loads content/*.php templates
-Backdrop\View\display( 'content', Backdrop\Template\Hierarchy\hierarchy() );
+Backdrop\View\display( 'content', Backdrop\Template\hierarchy() );
 
 // Loads footer/*.php templates
-Backdrop\View\display( 'footer', Backdrop\Template\Hierarchy\hierarchy() );
+Backdrop\View\display( 'footer', Backdrop\Template\hierarchy() );
